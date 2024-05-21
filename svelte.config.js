70fe28a81906bdb063248c3907b5a410895366ae
hg80 <hello@hg80.studio>
2024-05-21 21:59:42 +0300

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,7 @@
 import adapter from '@sveltejs/adapter-vercel';
 
-const config = {
+export default {
   kit: {
-    // Specify the adapter
-    adapter: adapter(),
-    // Specify the Node.js version
-    target: '#adapter',
-    // Optional: Set Node.js version
-    vite: {
-      ssr: {
-        noExternal: ['svelte']
-      }
-    },
-    // Optional: other kit configuration options
-  },
-  // Specify the Node.js version for the adapter
-  adapter: {
-    // Specify the Node.js version
-    node: '16', // or '18'
-  },
+    adapter: adapter()
+  }
 };
-
-export default config;
